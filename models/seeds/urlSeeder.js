b20baddb2bb998f9808759077ed0d7cfdbd5cbcd
user <carola1408@yahoo.com.tw>
2023-02-23 12:10:21 +0800

    feat: add url seeder 新增種子資料腳本

---
+++ models/seeds/urlSeeder.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose') // 載入 mongoose
+const url = require('../url')  // 載入 url model
+// 加入這段 code, 僅在非正式環境時, 使用 dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const urlShorteren = require('../../urlShorteren')
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+//新增資料
+db.once('open', () => {
+  console.log('mongodb connected')
+  const originalLinks = "http://www.goole.com"
+  const randomString = urlShorteren(5)
+  const host = "http://localhost:3000"
+  const shortLinks = host + "/" + randomString
+  url.create({
+    original_links: originalLinks,
+    short_links_random_string: randomString,
+    short_urls: shortLinks
+  })
+  console.log('done')
+})
---
+++ urlShorteren.js
@@ -0,0 +1,9 @@
+module.exports = (num) => {
+  const garbled = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  let result = ''
+  const garbledLength = garbled.length
+  for (let i = 0; i < num; i++) {
+    result += garbled.charAt(Math.floor(Math.random() * garbledLength))
+  }
+  return result
+}
