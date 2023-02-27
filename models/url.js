70da2a347eeceb48f15af8b8f9ff1ea24c60ad1c
user <carola1408@yahoo.com.tw>
2023-02-27 09:55:03 +0800

    feat: modify url.js

--- models/url.js
+++ models/url.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose') // 載入 mongoose
 const Schema = mongoose.Schema  //定義資料結構
 const urlSchema = new Schema({
-  original_links: {
+  url: {
     type: String, // 資料型別是字串
     required: true  // 這是個必填欄位
   },
-  short_links_random_string: {
+  shorterUrl: {
     type: String,
     require: true
   }
--- urlShortener.js
+++ urlShortener.js
@@ -1,4 +1,13 @@
-module.exports = (num) => {
+// module.exports = (num) => {
+//   const garbled = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+//   let result = ''
+//   const garbledLength = garbled.length
+//   for (let i = 0; i < num; i++) {
+//     result += garbled.charAt(Math.floor(Math.random() * garbledLength))
+//   }
+//   return result
+// }
+function urlShortener(times) {
   const garbled = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
   let result = ''
   const garbledLength = garbled.length
