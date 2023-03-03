9bbbc4da578af0cefccb8c10921d0bd401afc214
user <carola1408@yahoo.com.tw>
2023-03-03 16:59:36 +0800

    feat: modify url.js urlSeeder.js and app.js

--- app.js
+++ app.js
@@ -1,171 +1,71 @@
-// // 載入 express 並建構應用程式伺服器
-// const express = require('express')
-// const mongoose = require('mongoose') // 載入 mongoose
-// const exphbs = require('express-handlebars') // 載入 handlebars
-// const Url = require('./models/url') // 載入 url model
-// const bodyParser = require('body-parser') // 引用 body-parser
-// const urlShortener = require('./urlShortener')
-// const vaildUrl = require('valid-url')
-
-// // 加入這段 code, 僅在非正式環境時, 使用 dotenv
-// if (process.env.NODE_ENV !== 'production') {
-//   require('dotenv').config()
-// }
-
-// const app = express()
-
-// mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
-
-// // 取得資料庫連線狀態
-// const db = mongoose.connection
-// // 連線異常
-// db.on('error', () => {
-//   console.log('mongodb error!')
-// })
-// // 連線成功
-// db.once('open', () => {
-//   console.log('mongodb connected!')
-// })
-
-// //樣板引擎指定為 Handlebars
-// app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
-// app.set('view engine', 'hbs')
-
-// // setting static files設定靜態檔案路由
-// app.use(express.static('public'))
-
-// // 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
-// app.use(bodyParser.urlencoded({ extended: true }))
-
-// // 設定首頁路由
-// app.get('/', (req, res) => {
-//   Url.find() // 取出 Url model 裡的所有資料
-//     .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
-//     .then(url => res.render('index', { url })) // 將資料傳給 index 樣板
-//     .catch(error => console.error(error)) // 錯誤處理
-// })
-
-// //提醒防止沒有輸入內容就送出表單
-// app.get('/show-alarm', (req, res) => {
-//   res.send(`<script>
-//     alarm("Please enter the correct URL.");
-//     history.back();
-//   </script>`)
-// })
-
-// //產生短網址
-// app.post('/shorten', (req, res) => {
-//   const originalLinks = req.body.name
-//   if (!originalLinks || !vaildUrl.isWebUri(originalLinks)) {
-//     return res.redirect('/show-alarm')
-//   }
-//   Url.findOne({ url: originalLinks })
-//     .lean()
-//     .then(urlData => {
-//       if (!urlData) {
-//         const randomUrl = urlShortener(5)
-//         const host = req.get('origin')
-//         const shortLinks = host + '/' + randomUrl
-//         return Url.create({
-//           url: originalLinks,
-//           randomUrl: randomUrl,
-//           shorterUrl: shortLinks
-//         })
-//           .then(() => {
-//             res.render('newShorten', { shortLinks })
-//           })
-//       }
-//       res.render('shorten', { urlData })
-//     })
-//     .catch(error => console.error(error))
-// })
-
-// //使用短網址連向原始網站
-// app.get('/:shortLinks', (req, res) => {
-//   const shortLinks = req.params
-//   Url.findOne({ short_urls: shortLinks })
-//     .then(urlData => {
-//       if (!urlData) {
-//         return res.render('error', {
-//           errorMsg: '無法顯示網頁',
-//           errorLink: req.headers.host + '/' + shortLinks,
-//         })
-//       }
-//       res.redirect(urlData.url)
-//     })
-//     .catch(error => console.error(error))
-// })
-
-// // 設定 port 3000
-// app.listen(3000, () => {
-//   console.log('APP is running on http://localhost:3000')
-// })
-
-// require packages used in the project
+// 載入 express 並建構應用程式伺服器
 const express = require('express')
-const mongoose = require('mongoose')
-const exphbs = require('express-handlebars')
-const bodyParser = require('body-parser')
-
-const Urls = require('./models/url')
+const mongoose = require('mongoose') // 載入 mongoose
+const exphbs = require('express-handlebars') // 載入 handlebars
+const bodyParser = require('body-parser') // 引用 body-parser
+const Urls = require('./models/url') // 載入 url model
 const validUrl = require('valid-url')
-
 const urlShortener = require('./urlShortener')
 
-// require dotenv if NODE_ENV is not production
+//  加入這段 code, 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
 const app = express()
-const port = 3000
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
+// 取得資料庫連線狀態
 const db = mongoose.connection
-
+// 連線異常
 db.on('error', () => {
   console.log('mongodb error!')
 })
-
+// 連線成功
 db.once('open', () => {
   console.log('mongodb connected!')
 })
 
-// setting template engine
+// 樣板引擎指定為 Handlebars
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+//用 app.use 規定每一筆請求都需要透過 body - parser 進行前置處理
 app.use(bodyParser.urlencoded({ extended: true }))
 
-// setting static files
+//  setting static files設定靜態檔案路由
 app.use(express.static('public'))
 
-// routes setting
+//  設定首頁路由
 app.get('/', (req, res) => {
-  res.render('index')
+  Urls.find() // 取出 Url model 裡的所有資料
+    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .then(urls => res.render('index', { urls })) // 將資料傳給 index 樣板
+    .catch(error => console.error(error)) // 錯誤處理
 })
 
-app.get('/show-alert', (req, res) => {
+app.get('/show-alarm', (req, res) => {
   res.send(`<script>
     alert("Please enter the correct URL.");
     history.back();
   </script>`)
 })
 
+//產生短網址
 app.post('/shorten', (req, res) => {
   const originalLinks = req.body.name
 
-  // pop-up alert if no URL inputted
+  //提醒防止沒有輸入內容就送出表單
   if (!originalLinks || !validUrl.isWebUri(originalLinks)) {
-    return res.redirect('/show-alert')
+    return res.redirect('/show-alarm')
   }
 
-  // check whether the original link is already in the database
+  //check  original link 是否在資料庫
   Urls.findOne({ original_links: originalLinks })
     .lean()
     .then(urlsData => {
       if (!urlsData) {
-        // generate the new short links
+        // 產生短網址
         const randomString = urlShortener(5)
         const host = req.get('origin')
         const shortLinks = host + "/" + randomString
@@ -183,14 +83,15 @@ app.post('/shorten', (req, res) => {
     .catch(error => console.log(error))
 })
 
+//使用短網址連向原始網站
 app.get("/:shortLinks", (req, res) => {
   const { shortLinks } = req.params
   Urls.findOne({ short_links_random_string: shortLinks })
     .then(urlsData => {
       if (!urlsData) {
-        // render error page if can't found the url
+        // 錯誤網址產生錯誤網頁
         return res.render("error", {
-          errorMsg: "Oops! Page Not Found",
+          errorMsg: "無法顯示網頁",
           errorLink: req.headers.host + "/" + shortLinks,
         })
       }
@@ -199,7 +100,7 @@ app.get("/:shortLinks", (req, res) => {
     .catch(error => console.error(error))
 })
 
-// start and listen on the Express server
-app.listen(port, () => {
-  console.log(`App is running on http://localhost:${port}`)
+// 設定 port 3000
+app.listen(3000, () => {
+  console.log('APP is running on http://localhost:3000')
 })
--- models/seeds/urlSeeder.js
+++ models/seeds/urlSeeder.js
@@ -1,6 +1,6 @@
-const mongoose = require('mongoose') // 載入 mongoose
+const mongoose = require('mongoose') // 載入 mongoose 
+const Urls = require('../url') // 載入 url model
 
-const Urls = require('../url')  // 載入 url model
 // 加入這段 code, 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -20,49 +20,17 @@ db.on('error', () => {
 db.once('open', () => {
   console.log('mongodb connected!')
 })
-
 //新增資料
 db.once('open', () => {
-  console.log('mongodb connected')
+  console.log('mongodb connected!')
   const originalLinks = "https://tw.yahoo.com/"
-  const randomUrl = urlShortener(5)
+  const randomString = urlShortener(5)
   const host = "http://localhost:3000"
-  const shortLinks = host + "/" + randomUrl
+  const shortLinks = host + "/" + randomString
   Urls.create({
-    originalUrl: originalLinks,
-    randomUrl: randomUrl,
-    shorterUrl: shortLinks
+    original_links: originalLinks,
+    short_links_random_string: randomString,
+    short_urls: shortLinks
   })
   console.log('done')
 })
-
-
-// const mongoose = require('mongoose')
-// const Urls = require('../url')
-// if (process.env.NODE_ENV !== 'production') {
-//   require('dotenv').config()
-// }
-
-// const urlShortener = require('../../urlShortener')
-
-// mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
-
-// const db = mongoose.connection
-
-// db.on('error', () => {
-//   console.log('mongodb error!')
-// })
-
-// db.once('open', () => {
-//   console.log('mongodb connected!')
-//   const originalLinks = "https://www.google.com.hk/"
-//   const randomString = urlShortener(5)
-//   const host = "http://localhost:3000"
-//   const shortLinks = host + "/" + randomString
-//   Urls.create({
-//     original_links: originalLinks,
-//     short_links_random_string: randomString,
-//     short_urls: shortLinks
-//   })
-//   console.log('done')
-// })
